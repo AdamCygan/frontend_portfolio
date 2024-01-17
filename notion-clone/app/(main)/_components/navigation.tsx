0c0a9d95aee84cfd78d3326151a6d0834b3950c9
Adam Cygan <adamcyg772@student.polsl.pl>
2024-01-17 09:22:21 +0100

    fix: fixed redirection after creation of a new document and after deletion

--- notion-clone/app/(main)/_components/navigation.tsx
+++ notion-clone/app/(main)/_components/navigation.tsx
@@ -10,7 +10,7 @@ import {
   Settings,
   Trash,
 } from "lucide-react";
-import { useParams, usePathname } from "next/navigation";
+import { useParams, usePathname, useRouter } from "next/navigation";
 import { ElementRef, useRef, useState, useEffect } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { UserItem } from "./user-item";
@@ -29,6 +29,7 @@ import { Navbar } from "./navbar";
 export const Navigation = () => {
   const pathname = usePathname();
   const params = useParams();
+  const router = useRouter();
   const isMobile = useMediaQuery("(max-width:768px)");
 
   const create = useMutation(api.documents.create);
@@ -121,7 +122,7 @@ export const Navigation = () => {
   const handleCreate = () => {
     const promise = create({
       title: "Untitled",
-    });
+    }).then((documentId) => router.push(`/documents/${documentId}`));
 
     toast.promise(promise, {
       loading: "Creating a new note...",
