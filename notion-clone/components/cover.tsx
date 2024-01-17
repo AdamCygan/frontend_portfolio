78fb49dfd9e782c5a2cad363491e414706e8b83d
Adam Cygan <adamcyg772@student.polsl.pl>
2024-01-17 08:18:33 +0100

    perf: dynamically removed cover image in edgestore

--- notion-clone/components/cover.tsx
+++ notion-clone/components/cover.tsx
@@ -9,6 +9,7 @@ import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { useParams } from "next/navigation";
 import { Id } from "@/convex/_generated/dataModel";
+import { useEdgeStore } from "@/lib/edgestore";
 
 interface CoverImageProps {
   url?: string;
@@ -18,8 +19,15 @@ export const Cover = ({ url, preview }: CoverImageProps) => {
   const params = useParams();
   const coverImage = useCoverImage();
   const removeImage = useMutation(api.documents.removeCoverImage);
+  const { edgestore } = useEdgeStore();
 
-  const onRemove = () => {
+  const onRemove = async () => {
+    if (url) {
+      await edgestore.publicFiles.delete({
+        url: url,
+      });
+    }
+
     removeImage({
       id: params.documentId as Id<"documents">,
     });
@@ -30,7 +38,7 @@ export const Cover = ({ url, preview }: CoverImageProps) => {
       className={cn(
         "relative w-full h-[35vh] group",
         !url && "h-[12vh]",
-        url && "bg-muted",
+        url && "bg-muted"
       )}
     >
       {!!url && <Image src={url} fill alt="Cover" className="object-cover" />}
