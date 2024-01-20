e1c944bda4aa4aafd77497828c44c5ab1f400a54
Adam Cygan <adamcyg772@student.polsl.pl>
2024-01-20 19:19:42 +0100

    feat: added board details page with board deletion option

--- trelo-clone/app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ trelo-clone/app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,10 +1,20 @@
-import { Navbar } from "../../_components/navbar";
+import { startCase } from "lodash";
 import { OrgControl } from "./_components/org-control";
+import { auth } from "@clerk/nextjs";
 
+export async function generateMetadata() {
+  const { orgSlug } = auth();
+  return {
+    title: startCase(orgSlug || "organization"),
+  };
+}
 const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
-  return <>
-  <OrgControl/>
-  {children}</>;
+  return (
+    <>
+      <OrgControl />
+      {children}
+    </>
+  );
 };
 
 export default OrganizationIdLayout;
