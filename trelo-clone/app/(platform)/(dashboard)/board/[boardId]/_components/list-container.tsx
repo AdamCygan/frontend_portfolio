d3a1d5197f3d1b81300f962910dd8ff540090191
Adam Cygan <adamcyg772@student.polsl.pl>
2024-01-22 09:11:49 +0100

    feat: added list header enabling changing list name

--- trelo-clone/app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ trelo-clone/app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -2,17 +2,28 @@
 
 import { ListWithCards } from "@/types";
 import { ListForm } from "./list-form";
+import { ListItem } from "./list-item";
+import { useEffect, useState } from "react";
 
-interface ListContainerProps{
-    data: ListWithCards[];
-    boardId: string;
-};
-
-export const ListContainer =({data, boardId}: ListContainerProps)=>{
-    return(
-        <ol>
-            <ListForm/>
-            <div className="flex-shrink-0 w-1"/>
-        </ol>
-    )
+interface ListContainerProps {
+  data: ListWithCards[];
+  boardId: string;
 }
+
+export const ListContainer = ({ data, boardId }: ListContainerProps) => {
+  const [orderedData, setOrderedData] = useState(data);
+
+  useEffect(() => {
+    setOrderedData(data);
+  }, [data]);
+
+  return (
+    <ol className="flex gap-x-3 w-full">
+      {orderedData.map((list, index) => {
+        return <ListItem key={list.id} index={index} data={list} />;
+      })}
+      <ListForm />
+      <div className="flex-shrink-0 w-1" />
+    </ol>
+  );
+};
